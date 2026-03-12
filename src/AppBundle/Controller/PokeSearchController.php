3072705f263b03602c59de7e8b384934a445f898
JuanJesusEuro26 <juan.jesus.sanchez@eurotransportcar.com>
2026-03-12 10:29:23 +0100

    Made feature pokesearch function correctly

--- src/AppBundle/Controller/PokeSearchController.php
+++ src/AppBundle/Controller/PokeSearchController.php
@@ -13,8 +13,43 @@ class PokeSearchController extends Controller{
     /**
      * @Route("index/PokeSearch", name="PokeSearch")
      */
-    public function pokeSearchAction(){
-        return $this->render("proyectfiles/index.html.twig");
+    public function pokeSearchAction(Request $request){
+        $nombreInput = strtolower(trim($request->request->get('pokemon')));
+    
+        if(empty($nombreInput)){
+            return new JsonResponse(array("error" => "ERROR: introduce el nombre de un pokemon."), 400);
+        }
+
+        $url = "https://pokeapi.co/api/v2/pokemon/" . $nombreInput;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // No imprime, guarda en variable
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Sigue redirecciones
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Evita problemas de certificados SSL localmente
+
+        $respuesta = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode === 200) { //Si el contenido se ha cargado correctamente (status 200)
+            $datos = json_decode($respuesta, true);
+            
+            $resultado= array(
+                'nombre'=> ucfirst($datos['name']),
+                'id'=> $datos['id'],
+                'tipo' => ucfirst($datos['types'][0]['type']['name']),
+                'foto' => $datos['sprites']['front_default']
+            );
+
+            return new JsonResponse($resultado);
+        } else if($httpCode === 404){
+            return new JsonResponse(array("error"=>"ERROR: pokemon ".$nombreInput." no encontrado"), 404);
+        }
+            
+    
+        
+        
     }
 
     
